43695df8b2ea833077bc86ae875e4be05fc72617
Terence Chen <chenjienan2009@gmail.com>
2021-04-12 00:58:40 -0700

    Use TextureRect for HP display

--- src/Screens/HealthUI.cs
+++ src/Screens/HealthUI.cs
@@ -5,8 +5,10 @@ public class HealthUI : Control
 {
   private int _health = 4;
   private int _maxHealth = 4;
+
   private Stats _plyayerStats;
-  private Label _label;
+  private TextureRect _heartEmpty;
+  private TextureRect _heartFull;
 
   [Export]
   public int Health
@@ -31,22 +33,35 @@ public class HealthUI : Control
     set
     {
       _maxHealth = Mathf.Max(value, 1);
+
+      // Update heart(s) for max health
+      if (_heartEmpty != null)
+      {
+        _heartEmpty.RectSize = new Vector2(
+            (float)Health * 15,
+          _heartEmpty.RectSize.y);
+      }
     }
   }
 
   public void SetHealth(int value)
   {
-    _health = Mathf.Clamp(value, 0, _maxHealth);
-    if (_label != null)
-    {
-      _label.Text = $"HP = {_health}";
-    }
+    Health = value;
+
+    // Update heart(s) for health
+    if (_heartFull != null)
+      _heartFull.RectSize = new Vector2(
+          (float)Health * 15,
+          _heartFull.RectSize.y);
+
   }
 
   public override void _Ready()
   {
     this._plyayerStats = GetNode<Stats>("/root/PlayerStats");
-    this._label = GetNode<Label>("Label");
+    this._heartEmpty = GetNode<TextureRect>("HeartEmpty");
+    this._heartFull = GetNode<TextureRect>("HeartFull");
+
     this._health = _plyayerStats.Health;
     this._maxHealth = _plyayerStats.MaxHealth;
     _plyayerStats.Connect("HealthChanged", this, "SetHealth");
